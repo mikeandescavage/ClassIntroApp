9a13c46b255d42de385c189a9594d98acb82230a
Michael Andescavage <michaelandescavage@cloudfitsoftware.com>
2020-06-10 13:21:28 -0700

    another checkin

--- ClassIntroApp/Program.cs
+++ ClassIntroApp/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
-
+﻿
 namespace ClassIntroApp
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GameStatement statementOne = new GameStatement();
+            statementOne.Statement = "I traveled around Japan for over two weeks.";
+            statementOne.IsLie = false;
+
+
+            Console.WriteLine($"{statementOne.Statement}");
         }
     }
 }
