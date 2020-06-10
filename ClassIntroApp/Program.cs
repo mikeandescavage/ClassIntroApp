a8fa2ef039ac0c03db96887d9b2729cbc84b8ec3
Michael Andescavage <michaelandescavage@cloudfitsoftware.com>
2020-06-10 13:10:58 -0700

    initial commit

---
+++ ClassIntroApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassIntroApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
